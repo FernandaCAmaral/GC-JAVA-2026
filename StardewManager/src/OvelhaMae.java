dcd1d5db17e3262c8381ba5fd29f0b62cb6a2b58
Fernanda Amaral <camaralfernanda@gmail.com>
2026-09-10 20:06:06 -0300

    (feat): adiciona classe OvelhaMae

---
+++ StardewManager/src/OvelhaMae.java
@@ -0,0 +1,43 @@
+public class OvelhaMae implements Animal {
+    private String nome;
+    private String raca;
+    private int idade;
+    private int quantidadeLa;
+
+    public OvelhaMae(String nome, String raca, int idade, int quantidadeLa) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.quantidadeLa = quantidadeLa;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String getRaca() {
+        return raca;
+    }
+
+    @Override
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String emitirSom() {
+        return "Mééé!";
+    }
+
+    @Override
+    public String coletarProduto() {
+        if (quantidadeLa == 0) {
+            return nome + " não tem lã pra coletar agora.";
+        }
+        String resultado = nome + " produziu " + quantidadeLa + " pedaço(s) de lã!";
+        quantidadeLa = 0;
+        return resultado;
+    }
+}
