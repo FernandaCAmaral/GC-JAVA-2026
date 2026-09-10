5ded9897766c3d088ce3cecb5c8b5c6dab241b06
Fernanda Amaral <camaralfernanda@gmail.com>
2026-09-10 19:52:29 -0300

    (feat): adiciona classe VacaMae

---
+++ StardewManager/src/VacaMae.java
@@ -0,0 +1,43 @@
+public class VacaMae implements Animal {
+    private String nome;
+    private String raca;
+    private int idade;
+    private int baldesDeLeite;
+
+    public VacaMae(String nome, int idade, String raca, int baldesDeLeite) {
+        this.nome = nome;
+        this.idade = idade;
+        this.raca = raca;
+        this.baldesDeLeite = baldesDeLeite;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String getRaca() {
+        return raca;
+    }
+
+    @Override
+    public String emitirSom() {
+        return "Muu!";
+    }
+
+    @Override
+    public String coletarProduto() {
+        if (baldesDeLeite == 0) {
+            return nome + " não tem leite pra coletar agora.";
+        }
+        String resultado = nome + " produziu " + baldesDeLeite + " balde(s) de leite!";
+        baldesDeLeite = 0;
+        return resultado;
+    }
+}
