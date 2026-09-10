8f97d4273e19d38fda96c20c5416562046e9656c
Fernanda Amaral <camaralfernanda@gmail.com>
2026-09-10 20:02:28 -0300

    (feat): adiciona classe PorcoMae

---
+++ StardewManager/src/PorcoMae.java
@@ -0,0 +1,44 @@
+public class PorcoMae implements Animal {
+    private String nome;
+    private String raca;
+    private int idade;
+    private int qntTrufas;
+
+    public PorcoMae(String nome, String raca, int idade, int qntTrufas) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.qntTrufas = qntTrufas;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String getRaca() {
+        return raca;
+    }
+
+    @Override
+    public int getIdade() {
+        return idade;
+    }
+
+    @Override
+    public String emitirSom() {
+        return "Có Cóó!";
+    }
+
+    @Override
+    public String coletarProduto() {
+        if (qntTrufas == 0) {
+            return nome + " não tem ovos pra coletar agora.";
+        }
+        String resultado = nome + " produziu " + qntTrufas + " ovo(s)!";
+        qntTrufas = 0;
+        return resultado;
+    }
+
+}
